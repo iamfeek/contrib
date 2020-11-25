4d05f639166f5eeb4159120eb522219526baffad
Syafiq Hanafee <sy@fiq.me>
2020-11-25 14:33:31 +0800

    feat(CustomSelectBoxes): added description to template

--- src/components/selectboxes/CustomSelectBoxes.form.ts
+++ src/components/selectboxes/CustomSelectBoxes.form.ts
@@ -1,5 +1,6 @@
 import nestedComponentForm from 'formiojs/components/_classes/nested/NestedComponent.form';
 import CustomSelectBoxesDataDisplay from './editForm/CustomSelectBoxes.edit.data';
+import CustomSelectBoxesValidationDisplay from './editForm/CustomSelectBoxes.edit.validation';
 
 export default function (...extend) {
   return nestedComponentForm([
@@ -7,6 +8,16 @@ export default function (...extend) {
       key: 'data',
       label: 'Data',
       components: CustomSelectBoxesDataDisplay,
+    },
+    {
+      key: 'validation',
+      label: 'Validation',
+      components: CustomSelectBoxesValidationDisplay,
+    },
+    {
+      key: 'api',
+      label: 'API',
+      components: null
     }
   ], ...extend);
 }
